a833505d59a994aebf78277eafd5b40a3fef27f2
root <root@rp-imst-1.udistrital.edu.co>
2017-08-14 01:13:19 -0500

    version beta de los scripts

---
+++ install-all.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./install-libmpsse.sh            
+./install-packet-forwader.sh
+./install-lora-gateway-bridge.sh 
+
---
+++ install-packet-forwader.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git clone https://github.com/pylatesUD/lora_gateway
+cd lora_gateway
+git checkout ftdi
+make
+cd ..
+
+git clone https://github.com/pylatesUD/packet_forwarder
+cd packet_forwarder
+git checkout ftdi
+make
+cd ..
+
