d5b0d9497f65defadadeb96d1bcad349cda1bce4
root <root@rp-imst-1.udistrital.edu.co>
2017-09-15 16:22:50 -0500

    corrección de la mac

--- install-lora-packet-forwarder.sh
+++ install-lora-packet-forwarder.sh
@@ -6,18 +6,19 @@ cd lora_gateway
 git checkout ftdi
 make
 cd ..
-cp -ar lora_gateway/ /opt/
+cp -ar lora_gateway/ /opt/lora_gateway
 
 git clone https://github.com/pylatesUD/packet_forwarder
 cd packet_forwarder
 git checkout ftdi
 make
+
 cd basic_pkt_fwd/
 mac=$(cat /sys/class/net/eth0/address | sed -e 's/://g')
-eui="${mac:0:7}FFFE${mac: -6}"
+eui="${mac:0:6}FFFE${mac: -6}"
 sed -ie "s/AA555A003A0581D1/$eui/g" local_conf.json
 cd ../..
-cp -ar packet_forwarder/ /opt/
+cp -ar packet_forwarder/ /opt/packet_forwarder
 cd ..
 
 cp lora-packet-forwarder-openrc.sh /etc/init.d/lora-packet-forwarder
